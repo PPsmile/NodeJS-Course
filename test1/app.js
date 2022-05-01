e87856515e239e969efe38e512cb8faf9ff7f8ba
PPsmile <p.fifa2012@gmail.com>
2022-05-01 21:38:21 +0700

    add router

--- test1/app.js
+++ test1/app.js
@@ -3,17 +3,32 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router();
+const products = require('../data/products.json');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
+//app.use(express.static(path.join(__dirname,"/public/")));
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-app.get("/", (req,res) => {
-    res.send('Hello World');
+productRouter.route("/").get((req,res) => {
+    res.render("products", 
+        products,
+    );
+})
+productRouter.route("/1").get((req,res) => {
+    res.send("Hello Products 1");
 })
 
-app.listen(port, ()=>{
-    debug("lisenting on port" + chalk.green(port));
+app.use("/products", productRouter);
+
+app.get("/", (req,res) => {
+    res.render('index',{username: 'Inwza_007', customer: ['man-city', 'liverpool','barcelona']});
+})
+
+app.listen(PORT, ()=>{
+    debug("lisenting on port" + chalk.green(PORT));
 })
